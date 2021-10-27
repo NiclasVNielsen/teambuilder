3ab0f8fa669fe07bcf5fc4a9aee2c2f0df2a01d2
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-10-27 21:07:42 +0200

    tournament page display

--- src/views/Tournaments.vue
+++ src/views/Tournaments.vue
@@ -2,12 +2,48 @@
   <div>
       Tournaments
   </div>
+
+  <div v-for="tournament in tournaments" :key="tournament">
+      <div>
+        {{ tournament['name'] }}
+      </div>
+      <div>
+        {{ tournament['desc'] }}
+      </div>
+      <div>
+        {{ tournament['amountOfTeams'] }}
+      </div>
+      <div>
+        {{ tournament['icon'] }}
+      </div>
+      <div>
+        Sign up before: {{ tournament['signUpDeadline'].toDate().toDateString() }}
+      </div>
+      <div>
+        Starts: {{ tournament['time'].toDate().toDateString() }}
+      </div>
+      <div>
+          Teams:
+          <div v-for="team in tournament['teams']" :key="team">
+              {{ team }}
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { getAllTournaments } from '@/main.js'
+
 export default{
     setup() {
-        
+        let tournaments = ref('')
+
+        getAllTournaments()
+            .then(data => {
+                tournaments.value = data
+            }) 
+        return { tournaments }
     }
 }
 </script>
