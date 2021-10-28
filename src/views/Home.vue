ba96f8958321350aeac35a0226f96618934cbaad
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2021-10-28 14:37:32 +0200

    to do list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   This nerd likes to play {{ champion }}
+
+  <br>
+  
+  <ul>
+    <li>Make Teams</li>
+    <li>Invite player to team</li>
+    <li>Make Tournaments</li>
+    <li>Team sign up to tournament</li>
+    <li>Edit profile</li>
+    <li>tournament control wins and loses</li>
+  </ul>
 </template>
 
 <script>
