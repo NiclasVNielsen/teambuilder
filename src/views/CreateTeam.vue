2ba7ef46cacbcf3a6a39864b80ae20e27ac8da2e
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-10-28 16:23:52 +0200

    Basic form to console connection

--- src/views/CreateTeam.vue
+++ src/views/CreateTeam.vue
@@ -1,3 +1,42 @@
 <template>
-    Meep
+    <div class="login">
+        <div>
+            <h1>
+                Create Team
+            </h1>
+            <form @submit.prevent="createTeam">
+                <label for="name">Team Name</label>
+                <input type="text" name="name" id="name" placeholder="name" v-model="name">
+                <button type="submit">Create Team</button>
+            </form>
+        </div>
+    </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+import firebase from 'firebase/compat/app'
+
+export default{
+    setup() {
+        const name = ref('')
+
+        const createTeam = () => {
+            console.log(name.value)
+        }
+
+        //Nope you don't belong here check
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (!user) {
+                window.location.href = '/login'
+            }
+        });
+
+        return {
+            createTeam,
+            name
+        }
+    },
+    name: 'Login',
+}
+</script>
